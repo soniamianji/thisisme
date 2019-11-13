a255461a60497624c3b38ec1d83ae006b2df5ab3
fxrl <f.rossel@gmx.de>
2019-11-13 14:18:23 +0100

    sidebar

---
+++ client/src/components/child/SideBar.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Drawer } from "@material-ui/core";
+
+export default class SideBar extends Component {
+  render() {
+    return <div></div>;
+  }
+}
--- client/src/components/page/Profile.js
+++ client/src/components/page/Profile.js
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import UserCard from "../child/UserCard";
 import Container from "@material-ui/core/Container";
+import SideBar from "../child/SideBar";
+import { Drawer, Button, Grid, Typography, Box } from "@material-ui/core";
+import FontPicker from "font-picker-react";
 
-const Profile = () => (
-  <Container>
-    <UserCard />
-  </Container>
-);
+export default class Profile extends Component {
+  constructor(props) {
+    super(props);
 
-export default Profile;
+    this.state = {
+      open: false,
+      activeFontFamily: "Open Sans"
+    };
+
+    this.sideBarHandler = this.sideBarHandler.bind(this);
+  }
+
+  sideBarHandler() {
+    this.setState({ open: !this.state.open });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Button onClick={this.sideBarHandler}>Edit</Button>
+        <Drawer open={this.state.open} variant="persistent" anchor="left">
+          <Grid container xs={4}>
+            <Grid item>
+              <Box textAlign="left">
+                <Typography variant="h1">Edit Card</Typography>
+                <Typography variant="p">
+                  Change the typeface, design color, background color and add
+                  Links in the sidebar. To change the content (e.g. mail
+                  address) of your card by clicking directly on it and typing in
+                  the input field.
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item>
+              <FontPicker
+                apiKey="YOUR_API_KEY"
+                activeFontFamily={this.state.activeFontFamily}
+                onChange={nextFont =>
+                  this.setState({
+                    activeFontFamily: nextFont.family
+                  })
+                }
+              />
+            </Grid>
+          </Grid>
+        </Drawer>
+        <UserCard></UserCard>
+      </Container>
+    );
+  }
+}
