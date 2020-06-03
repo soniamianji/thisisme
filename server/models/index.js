a1471d1f39f3db9772de8075515cdaee6326c05d
Sonia <soniamianji1@gmail.com>
2020-06-03 10:12:41 +0200

    mongodb connection change

--- server/models/index.js
+++ server/models/index.js
@@ -11,15 +11,16 @@ module.exports.connect = uri => {
   // })
   // mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
   // // plug in the promise library:
-  // mongoose.Promise = global.Promise;
+
 
   // mongoose.connection.on("error", err => {
   //   console.error(`Mongoose connection error: ${err}`);
   //   process.exit(1);
   // });
-  mongoose.connect('mongodb://localhost:27017/creative_ideas', { useNewUrlParser: true })
+  mongoose.connect(uri, { useNewUrlParser: true })
     .then(() => console.log("Mongodb connected"))
     .catch(err => console.log(err));
+  //mongoose.Promise = global.Promise;
 
   // load models
   require("./card");
