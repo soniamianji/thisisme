f1c3c462e0a67be8b1c82f1210abcb39cb1ddeab
Sonia <soniamianji1@gmail.com>
2020-06-03 11:30:33 +0200

    mongodb test uri

--- server/models/index.js
+++ server/models/index.js
@@ -1,26 +1,19 @@
 const mongoose = require("mongoose");
 
 module.exports.connect = uri => {
-  // mongoose.connect(uri, (err, res) => {
-  //   if (err) {
-  //     console.log(err);
-  //   }
-  //   else {
-  //     console.log('connected');
-  //   }
-  // })
-  // mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  // // plug in the promise library:
 
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  // plug in the promise library:
+  mongoose.Promise = global.Promise;
 
-  // mongoose.connection.on("error", err => {
-  //   console.error(`Mongoose connection error: ${err}`);
-  //   process.exit(1);
-  // });
-  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Mongodb connected"))
-    .catch(err => console.log(err));
-  //mongoose.Promise = global.Promise;
+  mongoose.connection.on("error", err => {
+    console.error(`Mongoose connection error: ${err}`);
+    process.exit(1);
+  });
+  // mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  //   .then(() => console.log("Mongodb connected"))
+  //   .catch(err => console.log(err));
+
 
   // load models
   require("./card");
